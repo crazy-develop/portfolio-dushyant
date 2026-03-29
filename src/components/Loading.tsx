6f8aa791e2ca3e9d4e1492cb97e3e40c362727d9
crazy-develop <sainidushyant756@gmail.com>
2026-03-29 15:37:02 +0530

    fix: resolve infinite loop and Context Lost crash on loading screen 100%

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -10,14 +10,16 @@ const Loading = ({ percent }: { percent: number }) => {
   const [isLoaded, setIsLoaded] = useState(false);
   const [clicked, setClicked] = useState(false);
 
-  if (percent >= 100) {
-    setTimeout(() => {
-      setLoaded(true);
+  useEffect(() => {
+    if (percent >= 100) {
       setTimeout(() => {
-        setIsLoaded(true);
-      }, 500);
-    }, 300);
-  }
+        setLoaded(true);
+        setTimeout(() => {
+          setIsLoaded(true);
+        }, 500);
+      }, 300);
+    }
+  }, [percent]);
 
   useEffect(() => {
     import("./utils/initialFX").then((module) => {
